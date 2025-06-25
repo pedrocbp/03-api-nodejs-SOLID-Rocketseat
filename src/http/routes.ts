daa8113e0628d6933145c78e24987c9287348441
Pedro <pedromiguelboce@gmail.com>
2025-06-25 13:29:46 -0300

    feat: reestruturar o caso de uso de registro e reimplementação do repositório de usuários

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './register.controller'
+import { register } from './controllers/register.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
